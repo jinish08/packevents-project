0d64ada8479214a542696b34bbfe6391736f05a0
jinish08 <jinishshah08@gmail.com>
2025-09-22 12:01:49 -0400

    Initial project setup

---
+++ src/test/java/com/jinish/ncsu/sad/packeventsproject/PackeventsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jinish.ncsu.sad.packeventsproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PackeventsProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
